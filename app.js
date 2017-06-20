965cfe007e847c97c3f78394d5505cce659c4a7d
Michelle Scharlock <michelle.scharlock@gmail.com>
2017-06-19 22:03:37 -0700

    Colorful rainbow balloons are workinggit add .

--- app.js
+++ app.js
@@ -24,7 +24,79 @@ $(document).ready(function(){
   });
 });
 
-$(document).ready(function(){
-  $('div').animate()
+//random number range//
+function range( minNum, maxNum) {
+  return (Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum);
+}
 
-})
+function redBalloonFloat() {
+  for (var i = 0; i < 5; i++) {
+    var topRange = range(0, 960);
+    var leftRange = range(-1000, 960);
+
+    $('.allTheBalloons').append('<div class="redballoon" id="balloon'+i+'"></div>');
+    $('#balloon'+i).css('left', leftRange, 'background-color', 'red', 'top', topRange);
+  }
+}
+
+redBalloonFloat();
+
+function yellowBalloonFloat() {
+  for (var i = 0; i < 9; i++) {
+    var topRange = range(0, 960);
+    var leftRange = range(-1000, 960);
+
+    $('.allTheBalloons').append('<div class="yellowballoon" id="yballoon'+i+'"></div>');
+    $('#yballoon'+i).css('left', leftRange, 'background-color', 'yellow', 'top', topRange);
+  }
+}
+
+yellowBalloonFloat();
+
+function orangeBalloonFloat() {
+  for (var i = 0; i < 10; i++) {
+    var topRange = range(0, 960);
+    var leftRange = range(-1000, 960);
+
+    $('.allTheBalloons').append('<div class="orangeballoon" id="oballoon'+i+'"></div>');
+    $('#oballoon'+i).css('left', leftRange, 'background-color', 'orange', 'top', topRange);
+  }
+}
+
+orangeBalloonFloat();
+
+function greenBalloonFloat() {
+  for (var i = 0; i < 10; i++) {
+    var topRange = range(0, 960);
+    var leftRange = range(-1000, 960);
+
+    $('.allTheBalloons').append('<div class="greenballoon" id="gballoon'+i+'"></div>');
+    $('#gballoon'+i).css('left', leftRange, 'background-color', 'green', 'top', topRange);
+  }
+}
+
+greenBalloonFloat();
+
+function blueBalloonFloat() {
+  for (var i = 0; i < 10; i++) {
+    var topRange = range(0, 960);
+    var leftRange = range(-1000, 960);
+
+    $('.allTheBalloons').append('<div class="blueballoon" id="bballoon'+i+'"></div>');
+    $('#bballoon'+i).css('left', leftRange, 'background-color', 'blue', 'top', topRange);
+  }
+}
+
+blueBalloonFloat();
+
+function purpleBalloonFloat() {
+  for (var i = 0; i < 9; i++) {
+    var topRange = range(0, 960);
+    var leftRange = range(-1000, 960);
+
+    $('.allTheBalloons').append('<div class="purpleballoon" id="pballoon'+i+'"></div>');
+    $('#pballoon'+i).css('left', leftRange, 'background-color', 'purple', 'top', topRange);
+  }
+}
+
+purpleBalloonFloat();
